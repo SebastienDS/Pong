ad2f5253af9af389626f7a1e7e7a4b62cc6de0bd
Alan GARCIA <a.garcia.g007@gmail.com>
2020-06-20 15:28:15 +0200

    Added the main menu of the website

---
+++ public/include/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Menu Principal</title>
+    <link rel="stylesheet" href="./css/master.css">
+  </head>
+  <body>
+    <header>
+      <h1>Header</h1>
+    </header>
+    <?php include_once("navbar.php") ?>
